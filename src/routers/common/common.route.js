89390c8aa13ff48b4edf583f2e2efe43c6969473
Kzsanjeet <sanjeetkazithapa@gmail.com>
2026-03-09 21:57:04 +0545

    ft:added route for add destination

--- src/routers/common/common.route.js
+++ src/routers/common/common.route.js
@@ -2,23 +2,50 @@ import express from "express";
 
 const router = express.Router();
 
-import { acceptCustomizeBookingGuide,
-    addBulkDistrict, addSingleDistrict,
-    cancelCustomizeBookingUser, completeBookingGuide,
-
-    createCustomizeBooking, createGuideReport, createGuideReview, getAllBookings, getAllDistricts, getAllGuidesReports, getAllNotification, getAllUsers,
-    getBookings, getGuideReports, getOngoingBookings, getPopularDistricts,
-        getSingleDistrict, markAllNotificationAsRead, markNotificationAsRead, rejectCustomizeBookingGuide 
-    } from "../../controllers/common/index.js";
-import { getGuideReviews, getLatestGuideReviews } from "../../controllers/common/sub-controller/guideReviews.controller.js";
-
+import {
+  acceptCustomizeBookingGuide,
+  addBulkDistrict,
+  addSingleDistrict,
+  cancelCustomizeBookingUser,
+  completeBookingGuide,
+  createCustomizeBooking,
+  createGuideReport,
+  createGuideReview,
+  getAllBookings,
+  getAllDistricts,
+  getAllGuidesReports,
+  getAllNotification,
+  getAllUsers,
+  getBookings,
+  getGuideReports,
+  getOngoingBookings,
+  getPopularDistricts,
+  getSingleDistrict,
+  markAllNotificationAsRead,
+  markNotificationAsRead,
+  rejectCustomizeBookingGuide,
+} from "../../controllers/common/index.js";
+import {
+  getGuideReviews,
+  getLatestGuideReviews,
+} from "../../controllers/common/sub-controller/guideReviews.controller.js";
+import { addSingleDestination } from "../../controllers/common/sub-controller/destination.controller.js";
 
 //secure routes
 router.post("/create-customize-booking", createCustomizeBooking);
 router.get("/get-bookings/:id", getBookings);
-router.patch("/cancel-customize-booking-user/:bookingId", cancelCustomizeBookingUser);
-router.patch("/reject-customize-booking-guide/:bookingId", rejectCustomizeBookingGuide);
-router.patch("/accept-customize-booking-guide/:bookingId", acceptCustomizeBookingGuide);
+router.patch(
+  "/cancel-customize-booking-user/:bookingId",
+  cancelCustomizeBookingUser
+);
+router.patch(
+  "/reject-customize-booking-guide/:bookingId",
+  rejectCustomizeBookingGuide
+);
+router.patch(
+  "/accept-customize-booking-guide/:bookingId",
+  acceptCustomizeBookingGuide
+);
 router.get("/get-ongoing-booking/:id", getOngoingBookings);
 
 //district
@@ -29,6 +56,8 @@ router.get("/get-all-districts", getAllDistricts);
 router.get("/get-single-district/:slug", getSingleDistrict);
 router.get("/get-popular-districts", getPopularDistricts);
 
+//destinations
+router.post("/add-single-destination", addSingleDestination);
 
 //users
 router.get("/get-all-users/:userId", getAllUsers);
@@ -36,25 +65,28 @@ router.get("/get-all-users/:userId", getAllUsers);
 //booking complete
 router.patch("/complete-booking-guide/:bookingId", completeBookingGuide);
 
-
 //reviews
 router.post("/create-guide-review", createGuideReview);
 router.get("/get-guide-reviews/:guideId", getGuideReviews);
-router.get("/latest-guide-reviews", getLatestGuideReviews)
+router.get("/latest-guide-reviews", getLatestGuideReviews);
 
 //notification
 router.get("/all-notifications/:userId/:reciver", getAllNotification);
-router.patch("/mark-notification-as-read/:notificationId", markNotificationAsRead);
-router.patch("/mark-all-notification-as-read/:userId/:reciver", markAllNotificationAsRead);
+router.patch(
+  "/mark-notification-as-read/:notificationId",
+  markNotificationAsRead
+);
+router.patch(
+  "/mark-all-notification-as-read/:userId/:reciver",
+  markAllNotificationAsRead
+);
 
 //report
 router.post("/create-guide-report", createGuideReport);
 router.get("/get-guide-reports/:guideId", getGuideReports);
 router.get("/get-all-guides-reports", getAllGuidesReports);
 
-
 //admin
-router.get("/get-all-booking", getAllBookings)
-
+router.get("/get-all-booking", getAllBookings);
 
 export { router as commonRouter };
